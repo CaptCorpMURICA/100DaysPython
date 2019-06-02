df27db381d2589eeb6da5c26ad120728f6a2572b
Kevin Zufelt <23225223+CaptCorpMURICA@users.noreply.github.com>
2019-06-02 19:40:30 -0400

    Update module1_day11_loops.py

--- Module1/Day11/module1_day11_loops.py
+++ Module1/Day11/module1_day11_loops.py
@@ -1,8 +1,8 @@
 """
-    Author:         <REPLACE>
+    Author:         CaptCorpMURICA
     Project:        100DaysPython
     File:           module1_day11_loops.py
-    Creation Date:  <REPLACE>
-    Description:    <REPLACE>
+    Creation Date:  6/2/2019, 8:55 AM
+    Description:    Learn about loops in python.
 """
 
