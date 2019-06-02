2b01529472cd590314cded3021b504339967460d
Kevin Zufelt <23225223+CaptCorpMURICA@users.noreply.github.com>
2019-06-02 19:32:07 -0400

    Update module1_day01_installing.py

--- Module1/Day01/module1_day01_installing.py
+++ Module1/Day01/module1_day01_installing.py
@@ -1,8 +1,9 @@
 """
-    Author:         <REPLACE>
+    Author:         CaptCorpMURICA
     Project:        100DaysPython
     File:           module1_day01_installing.py
-    Creation Date:  <REPLACE>
-    Description:    <REPLACE>
+    Creation Date:  6/2/2019, 8:55 AM
+    Description:    Execute the Hello World program.
 """
 
+print("Hello World!")
