c87ccc5f3e3781ef2bafd02e0e8d2997b2144af4
Kevin Zufelt <23225223+CaptCorpMURICA@users.noreply.github.com>
2019-06-02 19:40:51 -0400

    Update module1_day12_augAssign.py

--- Module1/Day12/module1_day12_augAssign.py
+++ Module1/Day12/module1_day12_augAssign.py
@@ -1,8 +1,8 @@
 """
-    Author:         <REPLACE>
+    Author:         CaptCorpMURICA
     Project:        100DaysPython
     File:           module1_day12_augAssign.py
-    Creation Date:  <REPLACE>
-    Description:    <REPLACE>
+    Creation Date:  6/2/2019, 8:55 AM
+    Description:    Learn about augmented assignments in python.
 """
 
