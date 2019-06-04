62bc288cff46ddb21fccfd11621a7fd5c5c01dcc
CaptCorpMURICA <23225223+captcorpmurica@users.noreply.github.com>
2019-06-04 11:24:38 -0400

    Updated logic

--- Module1/Day14/module1_day14_challenge_soln.py
+++ Module1/Day14/module1_day14_challenge_soln.py
@@ -5,9 +5,9 @@
     Creation Date:  6/2/2019, 8:55 AM
     Description:    Create a program that iterates through a list of values. If the object is immutable, print the type
                     and advance to the next step. If the object is mutable and a string, add "Allegedly" to the end. If
-                    the object is mutable and a number, take 10 (int) to 20 (float) percent off, print the new value,
-                    and overwrite the value in the existing position. If an object is not a string, number, or tuple,
-                    end the program immediately while displaying the object and the type for review.
+                    the object is mutable and a number, take 10 (for an int) to 20 (for a float) percent off, print the
+                    new value, and overwrite the value in the existing position. If an object is not a string, number,
+                    or tuple, end the program immediately while displaying the object and the type for review.
 """
 
 content = ["Wayne is the toughest guy in Letterkenny.", list(range(0, 101, 10)), ("Wayne", "Dan", "Katy", "Daryl"),
@@ -25,19 +25,19 @@ for i in range(0, len(content)):
         updated_list = content[i]
         for j in range(0, len(updated_list)):
             if type(updated_list[j]) is int:
-                updated_list[j] = updated_list[j] - updated_list[j] * .1
+                updated_list[j] -= updated_list[j] * .1
             elif type(updated_list[j]) is float:
-                updated_list[j] = updated_list[j] - updated_list[j] * .2
+                updated_list[j] -= updated_list[j] * .2
                 updated_list.append(j)
             else:
                 updated_list.append(j)
         content[i] = updated_list
         print(content[i])
     elif type(content[i]) is int:
-        content[i] = content[i] - content[i] * .1
+        content[i] -= content[i] * .1
         print(content[i])
     elif type(content[i]) is float:
-        content[i] = content[i] - content[i] * .2
+        content[i] -= content[i] * .2
         print(content[i])
     else:
         print("{} is a {}, and nothing was done about it.".format(content[i], type(content[i])))
