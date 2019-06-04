3c8034ae44536e04622dcae88af985415de708e5
CaptCorpMURICA <23225223+captcorpmurica@users.noreply.github.com>
2019-06-04 11:07:05 -0400

    Content added 2019-06-04

--- Module1/Day13/module1_day13_continueBreak.py
+++ Module1/Day13/module1_day13_continueBreak.py
@@ -6,3 +6,39 @@
     Description:    Learn about continue/break operations in python.
 """
 
+# Print only consonants from a given text.
+motivation = "Over? Did you say 'over'? Nothing is over until we decide it is! Was it over when the Germans bombed " \
+             "Pearl Harbor? Hell no! And it ain't over now. 'Cause when the goin' gets tough...the tough get goin'! " \
+             "Who's with me? Let's go!"
+output = ""
+for letter in motivation:
+    if letter.lower() in 'bcdfghjklmnpqrstvwxyz':
+        output += letter
+print(output)
+
+#  However, this example can also be completed by using a continue action. If the condition is met, then the continue
+#  advances the program to the next stage.
+motivation = "Over? Did you say 'over'? Nothing is over until we decide it is! Was it over when the Germans bombed " \
+             "Pearl Harbor? Hell no! And it ain't over now. 'Cause when the goin' gets tough...the tough get goin'! " \
+             "Who's with me? Let's go!"
+output = ""
+for letter in motivation:
+    if letter.lower() not in 'bcdfghjklmnpqrstvwxyz':
+        continue
+    else:
+        output += letter
+print(output)
+
+# Conversely, the break action halts the program when the condition is met. For instance, what if we wanted to display
+# all of the letters until the first instance of a non-letter? A `break` can be used to end the loop once the condition
+# is met. _Type and execute:_
+motivation = "Over? Did you say 'over'? Nothing is over until we decide it is! Was it over when the Germans bombed " \
+             "Pearl Harbor? Hell no! And it ain't over now. 'Cause when the goin' gets tough...the tough get goin'! " \
+             "Who's with me? Let's go!"
+output = ""
+for letter in motivation:
+    if letter.lower() not in 'abcdefghijklmnopqrstuvwxyz':
+        output += letter
+    else:
+        break
+print(output)
--- Module1/Day14/module1_day14_challenge_soln.py
+++ Module1/Day14/module1_day14_challenge_soln.py
@@ -3,6 +3,42 @@
     Project:        100DaysPython
     File:           module1_day14_challenge_soln.py
     Creation Date:  6/2/2019, 8:55 AM
-    Description:    <REPLACE>
+    Description:    Create a program that iterates through a list of values. If the object is immutable, print the type
+                    and advance to the next step. If the object is mutable and a string, add "Allegedly" to the end. If
+                    the object is mutable and a number, take 10 (int) to 20 (float) percent off, print the new value,
+                    and overwrite the value in the existing position. If an object is not a string, number, or tuple,
+                    end the program immediately while displaying the object and the type for review.
 """
 
+content = ["Wayne is the toughest guy in Letterkenny.", list(range(0, 101, 10)), ("Wayne", "Dan", "Katy", "Daryl"),
+           10.4]
+
+for i in range(0, len(content)):
+    if type(content[i]) is tuple:
+        print("{} is a {}".format(content[i], type(content[i])))
+        continue
+    elif type(content[i]) is str:
+        content[i] += " Allegedly."
+        print(content[i])
+        continue
+    elif type(content[i]) is list:
+        updated_list = content[i]
+        for j in range(0, len(updated_list)):
+            if type(updated_list[j]) is int:
+                updated_list[j] = updated_list[j] - updated_list[j] * .1
+            elif type(updated_list[j]) is float:
+                updated_list[j] = updated_list[j] - updated_list[j] * .2
+                updated_list.append(j)
+            else:
+                updated_list.append(j)
+        content[i] = updated_list
+        print(content[i])
+    elif type(content[i]) is int:
+        content[i] = content[i] - content[i] * .1
+        print(content[i])
+    elif type(content[i]) is float:
+        content[i] = content[i] - content[i] * .2
+        print(content[i])
+    else:
+        print("{} is a {}, and nothing was done about it.".format(content[i], type(content[i])))
+        break
