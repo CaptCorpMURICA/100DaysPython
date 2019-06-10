5f1767e00a7be6d1c38d8d6729a67d8fd71b7fbf
Kevin Zufelt <23225223+CaptCorpMURICA@users.noreply.github.com>
2019-06-09 20:44:54 -0400

    Update and rename module3_day42_challenge.py to module3_day42_challenge_soln.py

--- Module3/Day42/module3_day42_challenge.py
+++
@@ -1,8 +0,0 @@
-"""
-    Author:         <REPLACE>
-    Project:        100DaysPython
-    File:           module3_day42_challenge.py
-    Creation Date:  <REPLACE>
-    Description:    <REPLACE>
-"""
-
---
+++ Module3/Day42/module3_day42_challenge_soln.py
@@ -0,0 +1,8 @@
+"""
+    Author:         CaptCorpMURICA
+    Project:        100DaysPython
+    File:           module3_day42_challenge_soln.py
+    Creation Date:  6/2/2019, 8:55 AM
+    Description:    <REPLACE>
+"""
+
