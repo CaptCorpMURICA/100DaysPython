78d2bb6f869e3c9b6942c459d7171f72211bdd12
Kevin Zufelt <23225223+CaptCorpMURICA@users.noreply.github.com>
2019-06-09 20:41:50 -0400

    Update and rename module2_day28_challenge.py to module2_day28_challenge_soln.py
    Updated 2019-06-09

---
+++ Module2/Day28/module2_day28_challenge_soln.py
@@ -0,0 +1,24 @@
+"""
+    Author:         CaptCorpMURICA
+    Project:        100DaysPython
+    File:           module2_day28_challenge_soln.py
+    Creation Date:  6/2/2019, 8:55 AM
+    Description:    Create a program to encrypt and decrypt messages using a substitution cipher. The user provides a
+                    word that does not have any duplicate letters and no numbers or special characters. The user then
+                    provides the instruction to either encrypt or decrypt the message. Finally, the user then provides
+                    the message to be encrypted or decrypted. Create a function to check if the transposition word is
+                    valid, another function to encrypt the message, and a third function to decrypt the message. When
+                    encrypting the message, only letters should be provided; do not return any spaces, numbers, or
+                    special character. Additionally, the program should not be case sensitive and all output should be
+                    in lowercase. A substitution cipher is created by shifting the letters of the alphabet based on a
+                    keyword. For instance, if the word provided is "zebra", the cipher would be:
+                    {"a": "z",
+                     "b": "e",
+                     "c": "b",
+                     "d": "r",
+                     "e": "a",
+                     "f": "c",
+                     "g": "d",
+                     etc.}
+"""
+
