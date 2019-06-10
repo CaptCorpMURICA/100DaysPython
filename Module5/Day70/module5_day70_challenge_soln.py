bd053269b6465d7950ec8cb073601c9e66644294
Kevin Zufelt <23225223+CaptCorpMURICA@users.noreply.github.com>
2019-06-09 20:45:52 -0400

    Update and rename module5_day70_challenge.py to module5_day70_challenge_soln.py

---
+++ Module5/Day70/module5_day70_challenge_soln.py
@@ -0,0 +1,8 @@
+"""
+    Author:         CaptCorpMURICA
+    Project:        100DaysPython
+    File:           module5_day70_challenge_soln.py
+    Creation Date:  6/2/2019, 8:55 AM
+    Description:    <REPLACE>
+"""
+
